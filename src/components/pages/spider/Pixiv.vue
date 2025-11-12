1b1988dc9b2d17540f2d5348b91eef1358394692
fordeslorn <159607131+fordeslorn@users.noreply.github.com>
2025-11-12 23:02:19 +0800

    add spider routers

---
+++ src/components/pages/spider/Pixiv.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1 class="text-2xl font-bold">Spider</h1>
+    <p>Pixiv Spider</p>
+  </div>
+</template>
---
+++ src/components/pages/spider/SpidersPage.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1 class="text-2xl font-bold">Spider</h1>
+    <p>Spider Page</p>
+  </div>
+</template>
