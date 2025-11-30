2518cb5637d78f7ad77714645da4f48616886d62
fordeslorn <159607131+fordeslorn@users.noreply.github.com>
2025-12-01 00:20:45 +0800

    bug fix

--- src/components/sidebar/SidebarBrand.vue
+++ src/components/sidebar/SidebarBrand.vue
@@ -18,8 +18,10 @@ defineProps<{
     <SidebarMenuItem>
       <SidebarMenuButton size="lg" class="pointer-events-none">
         <div class="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-          <img v-if="logo" :src="logo" alt="Logo" class="size-full object-contain" />
-          <component v-else-if="icon" :is="icon" class="size-5" />
+          <slot name="icon">
+            <img v-if="logo" :src="logo" alt="Logo" class="size-full object-contain" />
+            <component v-else-if="icon" :is="icon" class="size-5" />
+          </slot>
         </div>
         <div class="grid flex-1 text-left leading-tight">
           <span class="truncate font-semibold text-base">
